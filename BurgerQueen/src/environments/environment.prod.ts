d26f8949317e3d5a6c95ffe4a30170e3bb623a0d
YasmitDaysi <yasdayvasand98@mail.com>
2020-04-13 13:23:09 -0500

    firebase: {

--- BurgerQueen/src/environments/environment.prod.ts
+++ BurgerQueen/src/environments/environment.prod.ts
@@ -1,3 +1,13 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: "AIzaSyDAfQ03JyXe8PJvoC7bngbHFQ9vWEDRsao",
+  authDomain: "burger-queen-dce1c.firebaseapp.com",
+  databaseURL: "https://burger-queen-dce1c.firebaseio.com",
+  projectId: "burger-queen-dce1c",
+  storageBucket: "burger-queen-dce1c.appspot.com",
+  messagingSenderId: "240167255819",
+  appId: "1:240167255819:web:211037a116fddb0bbbec0e",
+  measurementId: "G-JTNSGRZHCX"
+  }
 };
